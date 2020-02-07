935bad25d53b30892fdcbefa09bf61e764645178
aleksi_ttu <aleksi@ttu.ee>
2020-02-07 19:18:25 +0200

    git

--- Demo1/README.md
+++ Demo1/README.md
@@ -1,1 +1,7 @@
 ## Demo1
+
+~~~
+npm install ts-loader typescript --save-dev
+
+npm install webpack webpack-cli webpack-dev-server --save-dev
+~~~
---
+++ Demo1/webpack.config.js
@@ -0,0 +1,30 @@
+'use strict';
+const path = require('path');
+module.exports = {
+    devServer: {
+        port: 8070,
+        compress: true,
+        contentBase: [
+            path.join(__dirname, 'dist'),
+            path.join(__dirname, 'wwwroot')
+        ]
+    },
+    // entry: {
+    //     main: './src/index.js',
+    // },
+    // output: {
+    //     filename: '[name].js',
+    //     path: path.resolve(__dirname, 'dist')
+    // },
+    // resolve: {
+    //     extensions: ['.ts', '.js', '.tsx']
+    // },
+    // module: {
+    //     rules: [
+    //         {
+    //             loader: 'ts-loader',
+    //             test: /\.tsx?$/
+    //         }
+    //     ]
+    // }
+};
