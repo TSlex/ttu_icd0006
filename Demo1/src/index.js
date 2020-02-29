698cf448d88b6ead8bf92a09e3c287694b66e6fe
aleksi_ttu <aleksi@ttu.ee>
2020-02-29 14:27:40 +0200

    demo3

--- Demo1/src/index.js
+++ Demo1/src/index.js
@@ -1,1 +1,20 @@
-'use strict';
+// 'use strict';
+
+// window.addEventListener("load", ev => {
+//     document.body.style.background = "red"
+// });
+
+// document.body.style.background = "red";
+// window.document.body.style.background = "red";
+
+// function test() {
+//     document.body.style.background = "red";
+// }
+
+window.addEventListener("load",
+    (
+        function () {
+            document.body.style.background = "red";
+        }
+    )()
+);
