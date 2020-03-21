8fd2475391818362e1fb438cd3a9ba43c35e8f0a
aleksi_ttu <aleksi@ttu.ee>
2020-03-21 15:24:27 +0200

    master

--- AureliaDemo/au-todo-app/src/app.ts
+++ AureliaDemo/au-todo-app/src/app.ts
@@ -1,3 +1,33 @@
+import { ITodo } from "domain/ITodo";
+
+
 export class App {
-  public message: string = 'Hello World!';
+    public message: string = 'Hello World!';
+    private _todos: ITodo[] = [];
+
+    private _placeholder = "What?";
+    private _appTitle = "Aurelia Todos";
+    private _submitButtonTitle = "Add";
+    private _input = "";
+
+    formSubmitted(event: Event) {
+        if (this._input.length > 0) {
+
+            this._todos.push({description: this._input, done: false})
+
+            // console.log(this.appState.getTodos());
+            //this.eventAggregator.publish(EventChannels.NewTodoCreation, this._input);
+            // this.appState.addTodo(this._input);
+            // console.log(this.appState.getTodos());
+        }
+        this._input = "";
+        event.preventDefault();
+    }
+
+    removeTodo(index: number){
+        if (index < 0 || index >= this._todos.length){
+            return;
+        }
+        this._todos.splice(index, 1)
+    }
 }
